9d60b792b82165ee76269f553c6ad08d2fd35655
Karl-Johan Karlsson <karl-johan.karlsson@liu.se>
2025-03-27 17:54:16 +0100

    Day 2 Rust solutions

---
+++ day-02/src/02-1.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Report {
+    levels: Vec<i32>,
+    differences: Vec<i32>,
+}
+
+impl Report {
+    fn new(levels: Vec<i32>) -> Report {
+        Report {
+            levels,
+            differences: vec![],
+        }
+    }
+
+    fn gen_differences(&mut self) {
+        self.differences = vec![];
+        for i in 0..(self.levels.len() - 1) {
+            self.differences.push(self.levels[i + 1] - self.levels[i]);
+        }
+    }
+
+    fn is_safe(&self) -> bool {
+        (self.differences.iter().all(|d| d.is_positive()) ||
+         self.differences.iter().all(|d| d.is_negative())) &&
+        self.differences.iter().all(|d| d.abs() >= 1 && d.abs() <= 3)
+    }
+}
+
+fn main() {
+    let safe_count =
+        fs::read_to_string("../02.input")
+        .unwrap()
+        .lines()
+        .map(|l| {
+            l.trim()
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+        })
+        .map(|l| {
+            let mut report = Report::new(l.collect());
+            report.gen_differences();
+            report.is_safe()
+        })
+        .filter(|safe| *safe)
+        .count();
+
+    println!("{safe_count}");
+}
---
+++ day-02/src/02-2.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Report {
+    levels: Vec<i32>,
+}
+
+impl Report {
+    fn new(levels: Vec<i32>) -> Report {
+        Report {
+            levels,
+        }
+    }
+
+    fn is_safe_levels(levels: &Vec<&i32>) -> bool {
+        let mut differences = vec![];
+        for i in 0..(levels.len() - 1) {
+            differences.push(levels[i + 1] - levels[i]);
+        }
+
+        (differences.iter().all(|d| d.is_positive()) ||
+         differences.iter().all(|d| d.is_negative())) &&
+        differences.iter().all(|d| d.abs() >= 1 && d.abs() <= 3)
+    }
+
+    fn is_safe(&self) -> bool {
+        self.levels.iter()
+            .combinations(self.levels.len() - 1)
+            .any(|l| Report::is_safe_levels(&l))
+    }
+}
+
+fn main() {
+    let safe_count =
+        fs::read_to_string("../02.input")
+        .unwrap()
+        .lines()
+        .map(|l| {
+            l.trim()
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+        })
+        .map(|l| {
+            let report = Report::new(l.collect());
+            report.is_safe()
+        })
+        .filter(|safe| *safe)
+        .count();
+
+    println!("{safe_count}");
+}
