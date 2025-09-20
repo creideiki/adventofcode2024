8339825a2d2f0a412e683a75230d5302c3f548bb
Karl-Johan Karlsson <creideiki@ferretporn.se>
2025-09-20 11:28:39 +0200

    Replace repository with notice of move to Codeberg

--- 03-1.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-input = File.read('03.input')
-
-inst = /mul\([[:digit:]]+,[[:digit:]]+\)/
-
-puts input.scan(inst).map { |mul| mul[4..-2].split(',').map(&:to_i).reduce(&:*) }.sum
